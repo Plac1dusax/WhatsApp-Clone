8e2e3045fcb3c6855fa54b0cde4f85c4267f0c2a
Furkan <FSDeryol@gmail.com>
2022-11-01 20:17:13 +0300

    archived logo and text added

--- src/components/FriendsListSection/FriendsListSection.js
+++ src/components/FriendsListSection/FriendsListSection.js
@@ -1,15 +1,21 @@
 import React from "react"
 import Navbar from "../Navbar/Navbar"
+import Archived from "../Archived/Archived"
 import SearchFriend from "../SearchFriend/SearchFriend"
 
 export default function FriendsListSection() {
   return (
     <>
-      <div>
-        <Navbar />
-      </div>
-      <div>
-        <SearchFriend />
+      <div className="navbar-and-searchbar-wrapper">
+        <div>
+          <Navbar />
+        </div>
+        <div>
+          <SearchFriend />
+        </div>
+        <div>
+          <Archived />
+        </div>
       </div>
     </>
   )
